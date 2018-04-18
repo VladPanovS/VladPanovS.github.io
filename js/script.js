6693b466df9b40fb5047b4a6e36e895d2bbe8e4d
VladPanovS <34751539+VladPanovS@users.noreply.github.com>
2018-04-18 23:00:17 +0300

    wood theme

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
- $(document).ready(function(){
-    $("#menu").on("click","a", function (event) {
-        event.preventDefault();
-        var id  = $(this).attr('href'),
-            top = $(id).offset().top;
-        $('body,html').animate({scrollTop: top}, 1500);
-    });
+$(document).ready(function(){
+  $('.header-slider').slick({
+    arrows: false,
+    dots: true,
+    fade: true,
+  });
 });
