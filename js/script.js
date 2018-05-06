79c033a295986ed7c3a95c23f2f39bf66faf1d65
VladPanovS <34751539+VladPanovS@users.noreply.github.com>
2018-05-06 21:01:34 +0300

    BEAUTY

--- js/script.js
+++ js/script.js
@@ -1,9 +1,25 @@
 $( document ).ready(function() {
-     new WOW().init();
-     $("#menu").on("click","a", function (event) {
-         event.preventDefault();
-         var id  = $(this).attr('href'),
-             top = $(id).offset().top;
-         $('body,html').animate({scrollTop: top}, 1500);
-     })
+             new WOW().init();
+    //scroll
+    $('.logo').click(function(){
+           $('html, body').animate({scrollTop:$('#top').position().top}, 2000);
+    });
+    $('.arrow-scroll').click(function(){
+           $('html, body').animate({scrollTop:$('#main-content').position().top}, 2000);
+    });
+    $('.school-link').click(function(){
+           $('html, body').animate({scrollTop:$('#education').position().top}, 2000);
+    });
+    $('.contact-link').click(function(){
+           $('html, body').animate({scrollTop:$('#contact').position().top}, 2000);
+    });
+
+    //burgrer menu
+             $(".burger").click(function(){
+             $(".burger-line").toggleClass("line-active")
+             $(".logo-nav").toggleClass("visible")
+             $(".visible").show();
+         });
+  
+
 });
